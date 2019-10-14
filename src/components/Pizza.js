4906d70f903535941486851063b1e5d5c6a9b6d4
tomjmcnamee <tom@tomjmcnamee.com>
2019-10-14 17:04:19 -0400

    changed color of delete button

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -7,7 +7,7 @@ const Pizza = (props) => {
       <td>{props.pizza.size}</td>
       <td>{props.pizza.vegetarian ? "Yes" : "No"}</td>
       <td><button onClick={() => props.sendToPizzaForm(props.pizza)} type="button" className="btn btn-primary">Edit Pizza</button></td>
-      {props.admin ? <td><button onClick={() => props.deletePizza(props.pizza)} type="button" className="btn btn-primary">Delete Pizza</button></td> : null }
+      {props.admin ? <td><button onClick={() => props.deletePizza(props.pizza)} type="button" className="btn btn-danger">Delete Pizza</button></td> : null }
 
     </tr>
   )
